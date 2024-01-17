c49eb1c1c5567b9f7780feafddc9fe13b39d358b
gg <156441215+terogest@users.noreply.github.com>
2024-01-17 13:53:29 -0500

    Update WelcomeBanner.tsx

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -100,8 +100,7 @@ export function WelcomeBanner() {
   return (
     <Welcome>
       <div>
-        <h1>Welcome To BrrSolCasino 
-        </h1>
+        <h1>Welcome To BrrSolCasino</h1>
         <p>
           A fair, simple and decentralized casino on Solana.
         </p>
