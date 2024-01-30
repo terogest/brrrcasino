6ecda7af5a57121d7e1b962056ddc3563bea7a54
gg <156441215+terogest@users.noreply.github.com>
2024-01-30 15:02:08 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -36,7 +36,7 @@ function Root() {
                 creator={PLATFORM_CREATOR_ADDRESS}
                 games={GAMES}
                 tokens={TOKENS}
-                defaultCreatorFee={0.05}
+                defaultCreatorFee={0.005}
                 defaultJackpotFee={0.001}
               >
                 <App />
